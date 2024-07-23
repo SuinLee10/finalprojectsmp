4d0790772cbfa668097797fc98b327ff368045f1
01 <talhwan@hotmail.com>
2024-07-23 15:51:30 +0900

    tbpost scroll2
    all-

---
+++ src/main/java/com/thc/smspr2/domain/Tbboard.java
@@ -0,0 +1,49 @@
+package com.thc.smspr2.domain;
+
+import com.thc.smspr2.dto.TbboardDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Table(indexes = {
+        @Index(columnList = "deleted")
+        ,@Index(columnList = "process")
+        ,@Index(columnList = "createdAt")
+        ,@Index(columnList = "modifiedAt")
+        //,@Index(columnList = "title")
+}) //인덱스 키가 유니크 키(여러개를 한번에 설정할때) 등을 설정할 수 있습니다.
+@Entity
+public class Tbboard extends AuditingFields {
+
+    //원래는 여기에 이렇게 선언합니다.
+    //하지만 공통으로 쓰는 필드들이 있으니, 상속을 해보겠습니다!!
+    /*@Id private String id;
+    @Setter private String deleted;
+    @Setter private String createdAt;
+    @Setter private String modifiedAt;*/
+
+    @Setter @Column(nullable = false, length=400) private String title;
+    @Setter @Column(nullable = false, length=400) private String author;
+    @Setter @Column(nullable = true, length=10000) @Lob private String content; // 본문
+
+    protected Tbboard(){}
+    private Tbboard(String title, String author, String content) {
+        this.title = title;
+        this.author = author;
+        this.content = content;
+    }
+    public static Tbboard of(String title, String author, String content) {
+        return new Tbboard(title, author, content);
+    }
+
+    public TbboardDto.CreateResDto toCreateResDto() {
+        //생성자로 리턴해도 되지만!1
+        /*
+        TbboardDto.CreateResDto createResDto1 = new TbboardDto.CreateResDto();
+        createResDto1.setId(this.getId());
+        */
+        //빌더를 쓰면 매우 깔끔!!
+        return TbboardDto.CreateResDto.builder().id(this.getId()).build();
+    }
+}
