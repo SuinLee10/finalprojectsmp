898ffed3ef1822bf66bd395afcdf732bde96dc54
01 <talhwan@hotmail.com>
2024-07-25 11:22:42 +0900

    tbuser base
    all-

---
+++ src/main/java/com/thc/smspr2/repository/TbuserRepository.java
@@ -0,0 +1,12 @@
+package com.thc.smspr2.repository;
+
+import com.thc.smspr2.domain.Tbuser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TbuserRepository extends JpaRepository<Tbuser, String> {
+    Tbuser findByUsernameAndPassword(String username, String password);
+}
